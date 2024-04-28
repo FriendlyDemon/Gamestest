cb927a9a5ec9b278445c556399afbeb9fa79911e
FriendlyDemon <henriquemichel@live.com>
2024-04-28 18:54:30 -0300

    update

--- base.js
+++ base.js
@@ -2,34 +2,36 @@ const rl = require('readline-sync')
 function d(num){
     return Math.floor(Math.random() * num)
 }
-const fire="fire damage", force="force damage",attack=Math.floor(Math.random() * 20),effect="this happens"
+function nome(obj){
+    return obj["name"]
+}
+fire="fire damage", force="force damage",attack=Math.floor(Math.random() * 20),effect="this happens",cantLvl=1
 
-const 
-firebolt=[
-name = "Firebolt",
-slot = 0,
-range = 120,
-spellType = attack,
-damage = 1*d(10),
-damageType = fire
-],
 
-magicMissile=[
-name = "Magic Missile",
-slot = 1,
-range = 120,
-spellType = effect,
-damage = (1*d(4))+1,
-damageType = force
-];
+firebolt={
+name : "Firebolt",
+slot : 0,
+range : 120,
+spellType : attack,
+damage : cantLvl*d(10),
+damageType : fire
+},
 
-const spells = [slot0 = [firebolt[0], "prestidigitation"], slot1 = [magicMissile[0], "shield"]];
+magicMissile={
+name : "Magic Missile",
+slot : 1,
+range : 120,
+spellType : effect,
+damage : (1*d(4))+1,
+damageType : force
+};
 
-function cast() {
+const spells = [slot0 = [firebolt ], slot1 = [magicMissile]];
+
+function castList() {
     spellSlots = ["cantrip", "1st level","2nd level","3rd level","4th level","5th level", "6th level", "7th level","8th level","9th level"]
     slot = rl.keyInSelect(spellSlots.slice(0,spells.length), "Wich slot would you like to cast?"),
-    castSpell = rl.keyInSelect(spells[slot], "Wich spell would you like to cast?")
-    console.log(spells[slot][castSpell])
+    castSpell = rl.keyInSelect(spells[slot].map(nome), "Wich spell would you like to cast?")
+    console.log(spells[slot][castSpell]["name"])
 }
-cast()
-console.log(d(10))
+castList()
---
+++ test.js
@@ -0,0 +1,11 @@
+/*
+var sample = new Array();
+sample[0] = 'one';
+sample[1] = 'two';
+var obj = {"sample": sample}
+
+var arrayname = "sample";
+var number = obj[arrayname][1];
+*/
+const test = {nome:"Henrique", idade:30,cabelo:"preto"}
+console.log(test["idade"])
