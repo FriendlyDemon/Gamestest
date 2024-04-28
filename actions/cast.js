7d53d288503d9912c17e9104006df32f6dd91f9b
FriendlyDemon <henriquemichel@live.com>
2024-04-28 17:12:26 -0300

    update

--- actions/cast.js
+++ actions/cast.js
@@ -1,7 +1,9 @@
-const spells = [slot0 = ["firebolt", "prestidigitation"], slot1 = ["magicMissile", "shield"]], spellSlots = ["cantrip", "1st level"], rl = require('readline-sync');
+const rl = require('readline-sync'),
+spells = [slot0 = ["firebolt", "prestidigitation"], slot1 = ["magicMissile", "shield"]];
 
 function cast() {
-    slot = rl.keyInSelect(spellSlots, "Wich slot would you like to cast?"),
+    spellSlots = ["cantrip", "1st level","2nd level","3rd level","4th level","5th level", "6th level", "7th level","8th level","9th level"]
+    slot = rl.keyInSelect(spellSlots.slice(0,spells.length), "Wich slot would you like to cast?"),
     castSpell = rl.keyInSelect(spells[slot], "Wich spell would you like to cast?")
     console.log("I cast",spells[slot][castSpell].toUpperCase()+"!")
 }
