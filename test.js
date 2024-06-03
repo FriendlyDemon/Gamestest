9d768e06e47e1dea197f8d852d715cc882ec9dad
FriendlyDemon <51927468+FriendlyDemon@users.noreply.github.com>
2024-06-03 14:43:34 -0300

    Update test.js

--- test.js
+++ test.js
@@ -9,3 +9,19 @@ var number = obj[arrayname][1];
 */
 const test = {nome:"Henrique", idade:30,cabelo:"preto"}
 console.log(test["idade"])
+/*
+  class Car {
+  constructor(name, year) {
+    this.name = name;
+    this.year = year;
+  }
+  age() {
+    const date = new Date();
+    return date.getFullYear() - this.year;
+  }
+}
+
+const myCar = new Car("Ford", 2014);
+document.getElementById("demo").innerHTML =
+"My car is " + myCar.age() + " years old.";
+  */
