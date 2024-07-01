9c4790a6f1e51e8a8bbb89d6a73e97a82b9acab1
FriendlyDemon <henriquemichel@live.com>
2024-07-01 13:57:57 -0300

    y

--- base.js
+++ base.js
@@ -7,16 +7,14 @@ function impSp(V1) {
     let { body } = require(`./spells/${V1}.js`)
     return body
 }
-function d(num) {
-    return Math.floor(Math.random() * num) + 1
-}
-let N = V1 => impSp(V1).name
+let d = num => Math.floor(Math.random() * num) + 1
+let sN = V1 => impSp(V1).name
 attack = (d(20))
 
 function castList(caster) {
     spellSlots = ["cantrip", "1st level", "2nd level", "3rd level", "4th level", "5th level", "6th level", "7th level", "8th level", "9th level"]
     slot = rl.keyInSelect(spellSlots.slice(0, impCh(caster).spells.length), "Wich slot would you like to cast?"),
-        castSpell = rl.keyInSelect(impCh(caster).spells[slot].map(N), "Wich spell would you like to cast?")
+        castSpell = rl.keyInSelect(impCh(caster).spells[slot].map(sN), "Wich spell would you like to cast?")
     console.log("I cast " + impSp(impCh(caster).spells[slot][castSpell]).name + "!")
 }
 
