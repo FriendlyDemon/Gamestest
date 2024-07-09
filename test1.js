4db39977e6cb1bafd791ac38955c2a0077177215
FriendlyDemon <henriquemichel@live.com>
2024-07-09 12:53:43 -0300

    y

--- test1.js
+++ test1.js
@@ -1,4 +1,11 @@
+/*
 function armor(x,y){
     return Math.floor(Math.pow(0.99,x)*y)
 }
 console.log(armor(185,1000))
+*/
+const fs = require('node:fs');
+const content = "isso é um teste";
+fs.writeFile('./test.json', content, err=>{})
+
+// https://nodejs.org/en/learn/manipulating-files/writing-files-with-nodejs
