7f73291601b2471e6d7922b39074a278a8685192
FriendlyDemon <51927468+FriendlyDemon@users.noreply.github.com>
2024-07-09 12:38:45 -0300

    Atualizar o test.js

--- test.js
+++ test.js
@@ -1,27 +1,3 @@
-/*
-var sample = new Array();
-sample[0] = 'one';
-sample[1] = 'two';
-var obj = {"sample": sample}
-
-var arrayname = "sample";
-var number = obj[arrayname][1];
-*/
-const test = {nome:"Henrique", idade:30,cabelo:"preto"}
-console.log(test["idade"])
-/*
-  class Car {
-  constructor(name, year) {
-    this.name = name;
-    this.year = year;
-  }
-  age() {
-    const date = new Date();
-    return date.getFullYear() - this.year;
-  }
-}
-
-const myCar = new Car("Ford", 2014);
-document.getElementById("demo").innerHTML =
-"My car is " + myCar.age() + " years old.";
-  */
+const js = require('node:js')
+let conteudo = "texto de teste"
+js.fileWriteSync('teste.json', conteudo, err=>{})
