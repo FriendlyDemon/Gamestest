64116441347672450ec0154c44248dde2cdce95f
FriendlyDemon <henriquemichel@live.com>
2024-07-10 13:47:25 -0300

    y

--- test1.js
+++ test1.js
@@ -5,13 +5,10 @@ function armor(x,y){
 console.log(armor(185,1000))
 */
 const fs = require('node:fs');
-function jsonfy(x1,x2) {
-    doc=require('./'+x1+x2).body
-    fs.writeFileSync('./',x2,'.json', JSON.stringify(doc), err=>{})}
+function jsonfy(x1,x2,x3) {
+    doc=require('./'+x1+"/"+x2+".js").body
+    for (i=0;i<=x3;i++){
+    fs.writeFileSync('./_Ref/'+x1+"/"+x2+i+'.json', JSON.stringify(doc), err=>{})}}
 
 // https://nodejs.org/en/learn/manipulating-files/writing-files-with-nodejs
-a=["characters","classes","habilities","items","races","spells"]
-b=[]
-for (let i=0; i<a.length(); i++) {
-    jsonfy(a[i])
-}
+jsonfy("characters","player",3)
