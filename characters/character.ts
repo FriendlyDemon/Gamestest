9447cac4c4501ebc258c65756a9f64c505576d63
Dev Jeff Alves <lucas-bbica@educar.rs.gov.br>
2024-07-11 11:56:00 -0300

    y

---
+++ characters/character.ts
@@ -0,0 +1,15 @@
+export class Character {
+  name:string race:string
+  classe:string charStatus: (strength:number,dexterity:number,constitution: number,inteligence: number,wisdom: number,charisma: number,)
+
+  level: 2
+  classes: 1,
+  xp: 100,
+
+  healthMax: 12,
+  gold: 20,
+  equipment: ["staff", "robe"],
+  inventory: ["gum", "rope", "slingshot"],
+
+  spells: [(slot0 = []), (slot1 = []),(slot2= []),(slot3= []),(slot4= []),(slot5= []),(slot6= []),(slot7= []),(slot8= []),(slot9= [])],
+};
