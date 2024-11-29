423a35878f84cbef1f528eb10c07511eb77b074b
senacrs <henriquemichel@live.com>
2024-11-29 16:23:50 -0300

    y

--- items/longsword.js
+++ items/longsword.js
@@ -3,6 +3,6 @@ module.exports = {
     type: "equipment",
     tags: ['one handed', 'martial', 'melee', 'versatile'],
     cost: 1500,
+    weight: 1.3,
     damage: [[1, 8, 10, 'slashing']]
 }
- 
---
+++ items/staff.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: "Staff",
+    type: "equipment",
+    tags: ['one handed', 'simple', 'melee', 'versatile', 'arcane focus'],
+    cost: 500,
+    weight: 1.8,
+    damage: [[1, 6, 8, 'bludgeoning']]
+}
