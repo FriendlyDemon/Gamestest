764d89e875bcedc0c7c3a16c116a27aee763738b
senacrs <henriquemichel@live.com>
2024-12-12 15:26:48 -0300

    y

--- characters/character.ts
+++ characters/character.ts
@@ -1,4 +1,4 @@
-export class character {
+export class Character {
   name: string;
   creatureType:string;
   race: string;
@@ -14,10 +14,10 @@ export class character {
   level: number;
   healthMax: number;
   gold: number;
-  equipment: object;
-  inventory: object;
+  equipment: Equip;
+  inventory: Inventory;
   spells?: {};
-  constructor(name:string,creatureType:string,race:string,str:number,dex:number,con:number,int:number,wis:number,cha:number,level:number,healthMax:number,gold:number,equip:object|string,inventory:object|string,spells?:object,classe?:string){
+  constructor(name:string,creatureType:string,race:string,str:number,dex:number,con:number,int:number,wis:number,cha:number,level:number,healthMax:number,gold:number,equip:Equip,inventory:Inventory,spells?:object,classe?:string){
     this.name=name;
     this.creatureType=creatureType;
     this.race=race;
@@ -26,8 +26,26 @@ export class character {
     this.level=level;
     this.healthMax=healthMax;
     this.gold=gold;
-    this.equipment=[equip];
-    this.inventory=[inventory];
+    this.equipment=equip;
+    this.inventory=inventory;
     this.spells=spells;
   };
 };
+
+class Equip{
+  head:string='empty'
+  back:string='empty'
+  torso:string='empty'
+  feet:string='empty'
+  hands:string='empty'
+  fingers:Array<string>=['empty']
+  neck:string='empty'
+  mainhand:string='empty'
+  offhand:string='empty'
+}
+
+class Inventory{
+  equipment:Array<string|void>=[]
+  materials:Array<string|void>=[]
+  misc:Array<string|void>=[]
+}
